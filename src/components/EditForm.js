ac0c2038f59084d36d3672cc74bb53a3a3ad8025
Aleksei <sudenkov.aleksei@mail.ru>
2021-10-12 14:31:15 +0300

    0.02

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -27,33 +27,8 @@ export default class EditForm extends Component {
                 deActivateEditMod()
             }}
             >
-                {editFlag ? <div>
-                    <div className='buttonSave'>
-                        <input type="submit" value="Save"/>
-                    </div>
-                        <div>
-                            <b>Name:  </b><input name='name' type='text' value={this.state.name}
-                                                onChange={this.handleNewFormValue}
-                        />
-                        </div>
-                        <div >
-                            <b>Email:  </b><input name='email' type='text' value={this.state.email}
-                                                 onChange={this.handleNewFormValue}
-                        />
-                        </div>
-                        <div>
-                            <b>Owner:  </b><input name='owner' type='text' value={this.state.owner}
-                                                 onChange={this.handleNewFormValue}
-                        />
-                        </div>
-                    </div>
-                    :
                     <div>
                         <div className='buttonEdit'>
-                        <button onClick={() => {
-                            this.props.activateEditMod()
-                        }}>Edit
-                        </button>
                         </div>
                         <div>
                             <b>Name: </b><span>{this.state.name}</span>
@@ -65,7 +40,6 @@ export default class EditForm extends Component {
                             <b>Owner: </b><span>{this.state.owner}</span>
                         </div>
                     </div>
-                }
             </form>
         )
     }
